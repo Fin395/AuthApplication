f3cc3e432447382328a03f374d2b3950726de69b
Sergei Finogenov <serfinogenov@yandex.ru>
2025-12-30 23:14:02 +0300

    creating the 'User' model

--- config/settings.py
+++ config/settings.py
@@ -83,11 +83,16 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
-
 USE_TZ = True
+
+# STATIC_URL = 'static/'
+# STATICFILES_DIRS = [BASE_DIR / 'static']
+#
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 STATIC_URL = 'static/'
