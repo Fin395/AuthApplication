7c59c3d6cca4e9eb54fffee82b958db4ad56ca2d
Sergei Finogenov <serfinogenov@yandex.ru>
2025-12-31 01:13:10 +0300

    updating the User model with the field 'name'

--- users/models.py
+++ users/models.py
@@ -4,15 +4,16 @@ from django.db import models
 
 class User(AbstractUser):
     username = None
-    email = models.EmailField(unique=True, verbose_name="Email")
+    name = models.CharField(max_length=50, blank=False, null=False, verbose_name='Имя')
+    email = models.EmailField(unique=True, verbose_name='Email')
 
 
-    USERNAME_FIELD = "email"
+    USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
     class Meta:
-        verbose_name = "Пользователь"
-        verbose_name_plural = "Пользователи"
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
 
     def __str__(self):
         return self.email
