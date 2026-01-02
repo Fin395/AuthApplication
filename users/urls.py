cb659dcd8db6cb0c20d9e6bb68f55454b4fc0cd2
Sergei Finogenov <serfinogenov@yandex.ru>
2026-01-02 15:00:10 +0300

    adding the token urls; updating the settings

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
 from users.views import UserCreateAPIView, UserUpdateAPIView, UserRetrieveAPIView, UserListAPIView, UserDestroyAPIView
@@ -11,5 +13,6 @@ urlpatterns = [
     path('user/<int:pk>/', UserRetrieveAPIView.as_view(), name='user-get'),
     path('user/users/', UserListAPIView.as_view(), name='user-list'),
     path('user/delete/<int:pk>/', UserDestroyAPIView.as_view(), name='user-delete'),
-
+    path("token/", TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name="token_obtain_pair",),
+    path("token/refresh", TokenRefreshView.as_view(permission_classes=(AllowAny,)), name="token_refresh",),
 ]
